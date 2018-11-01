af97657b8b1d7f5393c4e2dd5586f519c1b6392f
katiekopecky <kkopecky@wisc.edu>
2018-11-01 13:20:55 -0500

    some map classes added

---
+++ GWSAlumni/ClientApp/components/AlumniMap.tsx
@@ -0,0 +1,35 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import '../css/MapPageStyles.css';
+
+
+interface MapState {
+    
+}
+
+
+export default class AlumniMap extends React.Component<RouteComponentProps<{}>, MapState> {
+    constructor() {
+        super();
+        
+    }
+   
+    
+    render(){
+        const madison = {lat: 43.0731, lng: 89.4012};
+        
+        return  <div className={'map'}>
+                    {AlumniMap.initMap(madison)}
+                </div>;
+    }
+    
+    private static initMap(center:location){
+        var map = new google.maps.Map(, {zoom: 4, center: uluru});
+        return <h1>sucks </h1>;
+    }
+}
+
+interface location{
+    lat: number;
+    lng: number;
+}
---
+++ GWSAlumni/ClientApp/components/AlumniMapPage.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import AlumniMap from './AlumniMap';
+
+interface MapPageState {
+}
+
+export class AlumniMapPage extends React.Component<RouteComponentProps<{}>, MapPageState> {
+    constructor() {
+        super();
+        
+    }
+    render(){
+        return  <div>
+                    <h1>GWS Alumni</h1>
+                </div>;
+    }
+    
+}
